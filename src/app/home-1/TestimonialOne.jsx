80534cbc11828212f2dc6120979b956646125635
NMN3151 <nipunmadushankaint@gmail.com>
2026-01-27 14:38:20 +0530

    corrected the btn text

--- src/app/home-1/TestimonialOne.jsx
+++ src/app/home-1/TestimonialOne.jsx
@@ -449,6 +449,7 @@ function TestimonialOne() {
             color: white;
             box-shadow: 0 5px 15px rgba(200, 176, 138, 0.4);
           }
+            // dwadwadwad
 
           .see-more-btn:active {
             transform: translateY(0);
